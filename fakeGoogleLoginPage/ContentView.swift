6f7b9fd8e96282e6976fd9839f3bc53f30cbdc84
rodolfo comparsi <rodolfocomparsi@hotmail.com>
2022-11-11 06:47:27 -0300

    Initial Commit

---
+++ fakeGoogleLoginPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fakeGoogleLoginPage
+//
+//  Created by Rodolfo Comparsi on 11/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fakeGoogleLoginPage/fakeGoogleLoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  fakeGoogleLoginPageApp.swift
+//  fakeGoogleLoginPage
+//
+//  Created by Rodolfo Comparsi on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct fakeGoogleLoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
